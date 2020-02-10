0a7c013f201522c2fa775278ae28d0390c1becd4
Michael Thomas <michaelthomas1997n@hotmail.com>
2020-02-10 23:25:40 +0000

    Improved A* pathfinding and added gizmos to visualise

--- Assets/MichaelThomas/Scripts/PathFinding.cs
+++ Assets/MichaelThomas/Scripts/PathFinding.cs
@@ -4,62 +4,67 @@ using UnityEngine;
 
 public class PathFinding : MonoBehaviour
 {
-	//public Transform seeker, target;
+	public Transform seeker, target;
 
-    private const int MOVE_STRIAGHT_COST = 10;
-    private const int MOVE_DIAGONAL_COST = 14;
-
-	//private Node currentNode = new Node(new Vector2Int(5,5));
-	//private Node targetNode = new Node (new Vector2Int (9, 5));
+    public int MOVE_STRIAGHT_COST = 10;
+    public int MOVE_DIAGONAL_COST = 20;
 
     private World m_world;
+
+	public bool DebugCorners = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //hCost = ((targetNode.pos.x - currentNode.pos.x) * 10) + ((targetNode.pos.y - currentNode.pos.y) * 10);
-        //print(hCost);
         m_world = GetComponent<World>();
-//        Debug.Log(m_world.worldTiles[new Vector2Int(0,4)].mTileObject.transform.position);
-        Debug.Log(m_world.IsPositionWalkable(new Vector2Int(0, 2)));
-		//CLOSED.Add (currentNode);
-        //GetNeighbours();
-		CalculatePath(new Vector2Int(38, 12), new Vector2Int(48, 13));
+		CalculatePath(new Vector2Int(38, 12), new Vector2Int(82, 26));
     }
 
     // Update is called once per frame
     void Update()
     {
-		//CalculatePath (seeker.position, target.position);
+		CalculatePath(new Vector2Int(Mathf.RoundToInt(seeker.position.x), Mathf.RoundToInt(seeker.position.y)), new Vector2Int(Mathf.RoundToInt(target.position.x), Mathf.RoundToInt(target.position.y)));
     }
 
 	List<Node> GetNeighbours(Node currentNode)
     {
-        //for (int x = -1; x < 1; x++)
-        //{
-        //    for (int y = -1; y < 1; y++)
-        //    {
-        //        if (x == 0 && y == 0) continue;
-
-                
-        //    }
-        //}
 		List<Node> neighbours = new List<Node>();
 
-        for (int y = currentNode.pos.y - 1; y <= currentNode.pos.y + 1; y++)
-        {
-			if (y < -1)
-				continue;
-            for (int x = currentNode.pos.x - 1; x <= currentNode.pos.x + 1; x++)
+        for (int y = -1; y < 2; y++)
+        {	
+            for (int x = -1; x < 2; x++)
             {
-				if (!m_world.IsPositionWalkable (new Vector2Int (x, y)))
+				if (!m_world.IsPositionWalkable(new Vector2Int (x + currentNode.pos.x, y + currentNode.pos.y)))
+					continue;
+				if(x == 0 && y == 0)
 					continue;
 
-				if (x < -1)
-					continue;
+				Node newNeighbour = new Node(new Vector2Int(x + currentNode.pos.x, y + currentNode.pos.y));
 
-				Node newNeighbour = new Node(new Vector2Int(x, y));
-				neighbours.Add(newNeighbour);
+				if(DebugCorners && x != 0 && y != 0)
+				{
+					bool adjacentWall = false;
+
+					for(int v = -1; v < 2; v++)
+					{
+						if(v == 0) continue;
+
+						if(!m_world.IsPositionWalkable(newNeighbour.pos + new Vector2Int(0, v))) adjacentWall = true;
+					}
+
+					for(int h = -1; h < 2; h++)
+					{
+						if(h == 0) continue;
+
+						if(!m_world.IsPositionWalkable(newNeighbour.pos + new Vector2Int(h, 0))) adjacentWall = true;
+					}
+
+					if(!adjacentWall) neighbours.Add(newNeighbour);
+				}
+				else
+				{
+					neighbours.Add(newNeighbour);
+				}
             }
         }
 		return neighbours;
@@ -67,80 +72,112 @@ public class PathFinding : MonoBehaviour
 
 	int GetDistance(Node nodeA, Node nodeB)
 	{
-		int distX = Mathf.Abs (nodeA.pos.x - nodeB.pos.x);
-		int distY = Mathf.Abs (nodeA.pos.y - nodeB.pos.y);
+		int distX = Mathf.Abs(nodeA.pos.x - nodeB.pos.x);
+		int distY = Mathf.Abs(nodeA.pos.y - nodeB.pos.y);
 
 		if (distX > distY)
-			return 14 * distY + 10 * (distX - distY);
-		return 14 * distX + 10 * (distY - distX);
+			return MOVE_DIAGONAL_COST * distY + MOVE_STRIAGHT_COST * (distX - distY);
+		return MOVE_DIAGONAL_COST * distX + MOVE_STRIAGHT_COST * (distY - distX);
 	}
 
     public void CalculatePath(Vector2Int startPos, Vector2Int targetPos)
 	{
+		if (!m_world.IsPositionWalkable(targetPos) || !m_world.IsPositionWalkable(startPos))
+		{
+			Debug.Log("Target is out of bounds.");
+			return;
+		}
 		Node startNode = new Node(startPos);
 		Node targetNode = new Node (targetPos);
-		Debug.Log (startNode);
-		List<Node> OPEN = new List<Node>(); //Nodes to be checked
-		List<Node> CLOSED = new List<Node>(); //Nodes that have already been checked
-		OPEN.Add(startNode);
+		Dictionary<Vector2Int, Node> OPEN = new Dictionary<Vector2Int, Node>();
+		Dictionary<Vector2Int, Node> CLOSED = new Dictionary<Vector2Int, Node>();
+
+		OPEN.Add(startNode.pos, startNode);
+
+		float start_elapsedTime = Time.time;
 
 		while (OPEN.Count > 0) 
 		{
-			Node currentNode = OPEN [0];
-			currentNode.parent = startNode;
-			for (int i = 1; i < OPEN.Count; i++) {
-				if (OPEN[i].fCost < currentNode.fCost || OPEN[i].fCost == currentNode.fCost && OPEN[i].hCost < currentNode.hCost) 
+			Dictionary<Vector2Int, Node>.Enumerator currNode = OPEN.GetEnumerator();
+			currNode.MoveNext();
+
+			Node currentNode = currNode.Current.Value;
+			while(currNode.MoveNext())
+			{
+				if (currNode.Current.Value.fCost < currentNode.fCost || currNode.Current.Value.fCost == currentNode.fCost && currNode.Current.Value.hCost < currentNode.hCost) 
 				{
-					currentNode = OPEN[i];
+					currentNode = currNode.Current.Value;
 				}
 			}
 
-			OPEN.Remove(currentNode);
-			CLOSED.Add(currentNode);
+			OPEN.Remove(currentNode.pos);
+			CLOSED.Add(currentNode.pos, currentNode);
 
 			if (currentNode.pos == targetNode.pos) 
 			{
-				RetracePath (startNode, currentNode);
+				RetracePath(startNode, currentNode);
+				Debug.Log("A* Time taken to run " + (Time.time - start_elapsedTime));
+				OPEN.Clear();
+				CLOSED.Clear();
 				return;
 			}
 
 			foreach (Node neighbour in GetNeighbours(currentNode)) 
 			{
-				if (CLOSED.Contains(neighbour))
+				if (CLOSED.ContainsKey(neighbour.pos))
 					continue;
 				neighbour.gCost = GetDistance (neighbour, startNode);
 				neighbour.hCost = GetDistance(neighbour, targetNode);
 
-				if (!OPEN.Contains(neighbour)) 
+				if (!OPEN.ContainsKey(neighbour.pos)) 
 				{
 					neighbour.parent = currentNode;
-					OPEN.Add (neighbour);
+					OPEN.Add(neighbour.pos, neighbour);
 				}
 			}
-			
 		}
-    
 
-        //Calculate path in here
+		Debug.Log("Didn't hit target.");
+
+		OPEN.Clear();
+		CLOSED.Clear();
     }
 
-	/*void OnDrawGizmos()
+	void OnDrawGizmos()
 	{
-		OnDrawGizmos.DrawWireCube (transform.position, new Vector3 (100, 1, 100));
-		//add gizmos to display gizmos
-	}*/
+		Gizmos.color = Color.red;
+
+		for(int i = 0; i < gizmoPath.Count; i++)
+		{	
+			Gizmos.DrawWireCube(gizmoPath[i], new Vector3(1.0f, 1.0f));
+
+			if(i+1 < gizmoPath.Count)
+			{
+				Gizmos.DrawLine(gizmoPath[i], gizmoPath[i+1]);
+			}
+		}
+	}
+
+	public List<Vector3> gizmoPath = new List<Vector3>();
 
 	void RetracePath(Node startNode, Node targetNode)
 	{
+		gizmoPath.Clear();
+
 		List<Node> path = new List<Node>();
 		Node currentNode = targetNode;
-		while (currentNode.pos != startNode.pos) {
+
+		while(currentNode != startNode)
+		{
+			Vector3 pos = new Vector3(currentNode.pos.x, currentNode.pos.y, 0);
+			gizmoPath.Add(pos);
+
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
+
+		path.Add(startNode);
+
 		path.Reverse();
-		for (int i = 0; i < path.Count; i++) {
-			Debug.Log (path [i].pos);
-		}
 	}
 }
